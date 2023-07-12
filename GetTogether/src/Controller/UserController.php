3133a42898d76d6c71130f9f199520e15901830a
OpaniAlain <123356567+OpaniAlain@users.noreply.github.com>
2023-07-12 11:47:08 +0200

    add document end

--- GetTogether/src/Controller/UserController.php
+++ GetTogether/src/Controller/UserController.php
@@ -12,13 +12,7 @@ class UserController extends AbstractController
     #[Route('/user', name: 'app_user')]
     public function index(User $user): Response
     {
-        
-<<<<<<< Updated upstream
-        $user = new User;
-        
-=======
 
->>>>>>> Stashed changes
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
