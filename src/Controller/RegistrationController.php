f2128b4f4c4b475796074062772c3dcc713342f5
BrieucMasset <brieuc.masset28@gmail.com>
2023-07-13 17:27:03 +0200

    13/07 17h30

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Document\Users;
+use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/registration', name: 'app_registration')]
-    public function index(): Response
+    public function register(Request $request): Response
     {
+        $user = new Users();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form ->handleRequest($request);
+
         return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
