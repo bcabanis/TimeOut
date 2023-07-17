528fad50281ab3ac5f6bf60b7f4c23b1c524e7bb
BrieucMasset <brieuc.masset28@gmail.com>
2023-07-17 17:29:04 +0200

    17/07 17h30

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,16 +2,54 @@
 
 namespace App\Controller;
 
+use App\Document\Users;
+use App\Form\LoginFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/login')]
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    #[Route('/', name: 'app_login')]
+    public function login(Request $request): Response
     {
+        // Créer une nouvelle instance de l'entité Users
+        $user = new Users();
+
+        // Créer le formulaire d'inscription en utilisant LoginFormType et l'entité Users
+        $form = $this->createForm(LoginFormType::class, $user);
+
+        // Gère la soumission du formulaire
+        $form->handleRequest($request);
+
+
         return $this->render('login/index.html.twig', [
+            'loginForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/profil', name: 'app_profil')]
+    public function profil(): Response
+    {
+        return $this->render('login/profil.html.twig', [
+            'controller_name' => 'LoginController',
+        ]);
+    }
+
+    #[Route('/avatar', name: 'app_avatar')]
+    public function avatar(): Response
+    {
+        return $this->render('login/avatar.html.twig', [
+            'controller_name' => 'LoginController',
+        ]);
+    }
+
+    #[Route('/tags', name: 'app_tags')]
+    public function tags(): Response
+    {
+        return $this->render('login/tags.html.twig', [
             'controller_name' => 'LoginController',
         ]);
     }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,32 +4,42 @@ namespace App\Form;
 
 use App\Document\Users;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('lastname', TextType::class, [
-                'label' => 'Nom'
-            ])
-            ->add('firstname', TextType::class, [
-                'label' => 'Prénom'
-            ])
-            ->add('dateNaissance', DateType::class, [
-                'label' => 'Date de naissance'
-            ])
             ->add('email', EmailType::class, [
-                'label' => 'Email'
+                'label' => 'Adresse mail'
             ])
-            ->add('password', PasswordType::class, [
-                'label' => 'Mot de passe'
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent correspondre.',
+                'required' => true,
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer votre mot de passe'],
+            ])
+            ->add('CGU', CheckboxType::class, [
+                'label' => 'J\'accepte les CGU',
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter les conditions d\'utilisation pour créer un compte.'
+                    ]),
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'S\'inscrire',
+
             ]);
     }
 
