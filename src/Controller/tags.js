2672290d749a8b1860437376b99d611895f6cf28
Romain <132657704+RomainBaniska@users.noreply.github.com>
2023-07-17 11:22:06 +0200

    update

--- src/Controller/tags.js
+++ src/Controller/tags.js
@@ -1,19 +1,34 @@
+// L'idée d'avoir des tags sur Gettogether/Timeout va permettre à l'utilisateur de filtrer les événements qui l'intéressent en sélectionnant des tags prédéfinis.
+// Les événements restent apparents tant que les tags qui lui sont liés sont sélectionnés, autrement ils disparaissent.
 
-  // Fonction pour filtrer les événements en fonction des tags sélectionnés
-  function filtrerEvenements() {
-    // Récupère tous les tags
-    let tags = document.querySelectorAll('.tag');
+// La propriété .classList est utilisée pour manipuler les classes CSS d'un élément DOM (Document Object Model). Cela permet d'ajouter, supprimer ou vérifier la présence de classes sur un élément HTML.
 
-    // Efface la classe "selected" de tous les tags
+// La propriété .classList est un objet qui expose plusieurs méthodes utiles :
+
+// .add(): Cette méthode permet d'ajouter une classe à la liste des classes de l'élément.
+// .remove(): Cette méthode permet de supprimer une classe de la liste des classes de l'élément.
+// .toggle(): Cette méthode permet d'ajouter une classe si elle n'est pas présente, et de la supprimer si elle est déjà présente. C'est une manière pratique de basculer l'état d'une classe.
+// .contains(): Cette méthode permet de vérifier si une classe spécifique est présente sur l'élément. Elle renvoie true si la classe est présente et false sinon.
+// .item(): Cette méthode permet d'accéder à une classe spécifique dans la liste des classes en fonction de son index.
+// .replace(): Cette méthode permet de remplacer une classe par une autre dans la liste des classes de l'élément.
+
+
+// On réalise une fonction pour filtrer les événements en fonction des tags sélectionnés
+
+function filtrerEvenements() {
+// On récupère tous les tags
+let tags = document.querySelectorAll('.tag');
+
+// Efface la classe "selected" de tous les tags
     tags.forEach(function (tag) {
       tag.classList.remove('selected');
     });
 
-    // Ajoute la classe "selected" au tag cliqué
+    // On ajoute la classe "selected" au tag cliqué
     this.classList.add('selected');
 
     // Récupère les tags sélectionnés
-    var tagsSelection = document.querySelectorAll('.tag.selected');
+    let tagsSelection = document.querySelectorAll('.tag.selected');
 
     // Récupère tous les événements
     var evenements = document.querySelectorAll('.evenement');
