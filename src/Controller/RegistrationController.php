6808872aa84fa6d6656ea032e8adb82ed6843770
Romain <132657704+RomainBaniska@users.noreply.github.com>
2023-07-18 11:57:14 +0200

    Ajout du message d'erreur en cas de doublon mail en register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -28,6 +28,19 @@ class RegistrationController extends AbstractController
 
         // Vérifie si le formulaire a été soumis et s'il est valide
         if ($form->isSubmitted() && $form->isValid()) {
+            $email = $user->getEmail();
+
+            // Vérifier si l'adresse e-mail existe déjà dans la base de données
+            $existingUser = $userRepository->findOneBy(['email' => $email]);
+            if ($existingUser) {
+                // Si un utilisateur avec l'e-mail existe déjà, afficher une modal d'erreur
+                return $this->render('registration/error.html.twig', [
+                    'message2' => 'Un compte existe déjà avec cette adresse e-mail.',
+                ]);
+            }
+
+            // L'e-mail est unique, nous pouvons continuer avec l'enregistrement de l'utilisateur
+
             // Hashe le mot de passe de l'utilisateur en utilisant l'objet UserPasswordHasherInterface
             $hashedPassword = $userPasswordHasher->hashPassword($user, $user->getPassword());
             // Défini le mot de passe hashé dans l'objet Users
@@ -36,8 +49,10 @@ class RegistrationController extends AbstractController
             // Enregistre l'utilisateur dans la base de données en utilisant la fonction save dans le UserRepository
             $userRepository->save($user);
 
-            // Redirige vers une autre page pour afficher un message de succès
-            return $this->redirectToRoute('registration_success');
+            // Affiche la modal de succès
+            return $this->render('registration/success.html.twig', [
+                'message' => 'Votre compte a été créé.',
+            ]);
         }
 
         // Affiche le formulaire d'inscription à la vue Twig
@@ -47,12 +62,19 @@ class RegistrationController extends AbstractController
         ]);
     }
 
-
     #[Route('/registration-success', name: 'registration_success')]
     public function register_success(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         return $this->render('registration/success.html.twig', [
             'message' => 'Votre compte a été créé.',
+        ]);
+    }
+
+    #[Route('/registration-error', name: 'registration_error')]
+    public function register_error(): Response
+    {
+        return $this->render('registration/error.html.twig', [
+            'message2' => 'Un compte existe déjà avec cette adresse mail.',
         ]);
     }
 }
