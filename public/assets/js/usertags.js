40cc0526e237fdd3a491c84f20639096737ee729
Romain <132657704+RomainBaniska@users.noreply.github.com>
2023-07-26 12:27:57 +0200

    PAGE MESTAGS

--- public/assets/js/usertags.js
+++ public/assets/js/usertags.js
@@ -55,125 +55,62 @@ function handleTagChoiceClick(event) {
   // On appelle la fonction pour générer le contenu d'un tableau actif initialement
   ListeDeBoutons();
   
+  ////////////
 
-// usertags.js
+// On crée une fonction qui va servir à afficher ou faire disparaitre certains "articles" grace à la searchbar.
 
-//   // Fonction pour le traitement du clic sur un bouton tag-choice
-//   function handleTagChoiceClick(event) {
-//     const button = event.target; // Récupérer le bouton sur lequel le clic s'est produit
-//     toggleTagChoice(button); // Appeler la fonction pour basculer les classes
-//   }
+function handleCategorySelection() {
 
-//   // Fonction pour basculer les classes du bouton
-//   function toggleTagChoice(button) {
-//     // Vérifier si le bouton a la classe "tag-choice"
-//     if (button.classList.contains('tag-choice')) {
-//       button.classList.remove('tag-choice'); // Le bouton est "tag-choice", on le transforme en "tag-choice-active"
-//       button.classList.add('tag-choice-active');
-//     } else if (button.classList.contains('tag-choice-active')) {
-//       button.classList.remove('tag-choice-active'); // Le bouton est "tag-choice-active", on le transforme en "tag-choice"
-//       button.classList.add('tag-choice');
-//     }
-//   }
+  // Récupérer la balise select par son ID
+  const selectElement = document.getElementById('categories');
+  // Récupérer les options sélectionnées
+  const selectedOptions = Array.from(selectElement.selectedOptions);
+  // Créer un tableau pour stocker les valeurs sélectionnées 
+  const selectedValues = selectedOptions.map(option => option.value);
 
-//   // Récupérer tous les boutons avec la classe "tag-choice"
-//   const buttons = document.querySelectorAll('.tag-choice');
-//   let activeButtons = document.querySelectorAll('.tag-choice-active');
-//   // On met tous ces tags dans un tableau
-//   let activeButtonsArray = Array.from(activeButtons);
+  // Récupérer tous les articles dans la section mestags
+  const articles = document.querySelectorAll('#mestags article');
 
-//   //On va ajouter tous les événements dans un tableau pour récapituler tous ceux sélectionnés en bas de page
-//   let activeButtonsContainer = document.getElementById('active-buttons-container');
+  // Parcourir chaque article 
+  articles.forEach(article => {
+    // Récupérer chaque categorie de l'article (utiliser l'ID de l'article sans le préfixe "#")
+    const category = article.id;
 
-//   // On va générer le contenu du tableau actif 
-//   function ListeDeBoutons {
-//     // On crée une liste à puces pour afficher les éléments
-//     let MegaListe = document.createElement('ul');
-//     // Parcourir le tableau actif et créer un élément de liste (li) pour chaque élément
-//     activeButtonsArray.forEach(button => {
-//         let MegaListeItem = document.createElement('li');
-//         MegaListeItem.textContent = button.textContent; // On utilise le contenu du bouton comme texte de la liste
-//         ListeDeBoutons.appendChild(MegaListeItem); // On ajoute l'élément de liste à la liste à puce
-//     });
-//   }
+    // Vérifier si la categorie de l'article est incluse dans les valeurs sélectionnées 
+    if (selectedValues.includes (category)) {
+      article.style.display = 'block';
+      } else {
+        // Sinon, masquer l'article 
+      article.style.display = 'none';
+      }
+  });
+}
 
-//   // Ajouter un gestionnaire d'événements "click" à chaque bouton
-//   buttons.forEach(button => {
-//     button.addEventListener('click', handleTagChoiceClick);
-//   });
+  // Ajouter un gestionnaire d'événements "change" à la balise select avec l'ID "categories"
 
-//   // effacer le contenu précédent du conteneur 
-//   activeButtonsContainer.innerHTML = '';
-
-//   // ajouter la liste à puces au conteneur
-//   activeButtonsContainer.appendChild(list);
-
-//   // On appelle la fonction pour générer le contenu d'un tableau actif initialement
-//   ListeDeBoutons();
+  document.getElementById('categories').addEventListener('change', handleCategorySelection);
 
 
-
-
-
-
-// // Fonction pour "toggler" un tag
-// function toggleTagChoice(button) {
-//     // Vérifier si le bouton a la classe "tag-choice"
-//     if (button.classList.contains('tag-choice')) {
-//       button.classList.remove('tag-choice'); // Le bouton est "tag-choice", on le transforme en "tag-choice-active"
-//       button.classList.add('tag-choice-active');
-//     } else if (button.classList.contains('tag-choice-active')) {
-//       button.classList.remove('tag-choice-active'); // Le bouton est "tag-choice-active", on le transforme en "tag-choice"
-//       button.classList.add('tag-choice');
-//     }
-//   }
-
-
-// // On récupère et on crée une variable pour tous les boutons qui ont la classe "tag-choice" (ou #mestags button.tag-choice)
-// const allTags = document.querySelectorAll('.tag-choice');
-
-// // On ajoute un gestionnaire d'événement à chaque bouton
-// allTags = forEach(button => {
-//     button.addEventListener('click',() => {
-//         // On vérifie si le bouton a la classe "active" ou non
-//         const activeTags = button.classList.contains('active');
-//         if (activeTags) {
-//             button.classList.remove('lp'); // le bouton est actif, donc on le désactive (togglé en OFF)
-//         } else {
-//             button.classList.add('lp');
-
-//         }    
-// });
-// });
-
-// //Fonction pour mettre à jour la <div class = "empty"> avec les noms des tags sélectionnés
-// function updateSelectedTags() {
-//     const SelectedTags = [];
-//     const activeButtons = document.querySelectorAll('.tag-choice.active');
-
-//     activeButtons
-// }
-
-
-
-// // // Fonction pour enregistrer tous les tags "togglés dans une liste"
-// // function saveToggledTags() {
-
-// // }
-
-
-// // Fonction pour enregistrer les tags "togglés" dans le repository Users.
-// function EnregistrerTags() {
-//     let $SaveUserTags = document.getElementById("enregistrer-changements");
-// }
-
-// // Récupération du bouton par son ID
-// let seeMoreTagsBtn = document.getElementById('see-more-tags-btn');
-
-// // Attacher l'événement click
-// seeMoreTagsBtn.addEventListener('click', toggleTagsDropdown);
-
-// // Filtre les événements pour afficher tous les événements au chargement initial de la page
-// window.onload = function () {
-//     filterEventsBySelectedCategories();
-// };
+  function handleSearch() {
+    // Récupérer la valeur saisie dans la barre de recherche
+    const searchString = document.getElementById('tagsearch').value.toLowerCase();
+    // Récupérer tous les titres d'article (éléments h2) dans la section mestags
+    const articleTitles = document.querySelectorAll('#mestags article h2');
+    // Parcourir chaque titre d'article
+    articleTitles.forEach(title => {
+      // Récupérer le texte du titre d'article 
+      const titleText = title.textContent.toLowerCase();
+      // Vérifier si le texte du titre d'article contient la chaîne de caractères saisie dans la recherche
+      if (titleText.includes(searchString)) {
+        // Si le titre d'article contient la chaîne de caractères saisie, afficher l'article parent
+        title.parentElement.style.display = 'block';
+      } else {
+        // Sinon masquer l'article parent
+        title.parentElement.style.display = "none";
+      }
+    });
+  }
+  
+  // Ajouter un gestionnaire d'événements "input" à la barre de recherche avec l'ID "tagsearch"
+  document.getElementById('tagsearch').addEventListener('input', handleSearch);
+  
